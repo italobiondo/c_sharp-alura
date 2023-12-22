c9ce0e3cb4ea032606751f6e8868ce5135227ce0
Italo Biondo <66559009+italobiondo@users.noreply.github.com>
2023-12-22 13:11:27 -0300

    Algumas aulas feitas

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,59 @@
+﻿//Screen Sound
+
+string mensagemDeBoasVindas = "Seja bem vindo ao Screen Sound";
+
+
+void EscreveNaTela()
+{
+    Console.WriteLine(@"
+    
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoMenu = Console.ReadLine()!;
+    int opcoesMenuNumerico = int.Parse(opcaoMenu);
+
+    switch (opcoesMenuNumerico)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcoesMenuNumerico);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcoesMenuNumerico);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcoesMenuNumerico);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcoesMenuNumerico);
+            break;
+        case -1:
+            Console.WriteLine("Saindo do Screen Sound.");
+            break;
+        default: Console.WriteLine("Opção invalida.");
+            break;
+    }
+
+}
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+Console.WriteLine("Olá, {0}!", nome);
+
+EscreveNaTela();
+ExibirOpcoesMenu();
---
+++ adivinhacao/adivinhacao/Program.cs
@@ -0,0 +1,22 @@
+﻿//ADIVINHAÇÃO
+
+Random aleatorio = new Random();
+
+int numeroSecreto = aleatorio.Next(1,101);
+
+do {
+    Console.WriteLine("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número.");
+        break;
+    } else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é maior que o seu chute.");
+    } else
+    {
+        Console.WriteLine("O número secreto é menor que o seu chute.");
+    }
+} while (true);
