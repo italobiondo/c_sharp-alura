3aa5d7c7db9f063b72b6d3d19855c7e454e86c3f
Italo Biondo <66559009+italobiondo@users.noreply.github.com>
2023-12-27 16:10:26 -0300

    Novo projeto do Curso envolvendo Classes e afins

---
+++ ScreenSound 2/ScreenSound 2/Musica.cs
@@ -0,0 +1,24 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => 
+            $"A música {Nome} pertence a banda {Artista}.";//lambda
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nArtista: {Artista}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Não disponível para o seu plano.");
+        }
+    }
+}
---
+++ ScreenSound 2/ScreenSound 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Album albumDoLinkin = new Album();
+albumDoLinkin.Nome = "Hybrid Theory";
+
+Musica musica1 = new Musica();
+musica1.Nome = "In the End";
+
+Musica musica2 = new Musica();
+musica2.Nome = "Papercut";
+
+albumDoLinkin.AdicionarMusica(musica1);
+albumDoLinkin.AdicionarMusica(musica2);
+
+Console.WriteLine();
